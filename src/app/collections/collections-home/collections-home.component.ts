76ea1fcc421de195ed713cc7a79401930748d583
Leonardo Gonçalves <leomcg@gmail.com>
2020-06-15 09:53:40 -0300

    created reusable table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer', employed: false },
+    { name: 'Jill', age: 32, job: 'Astronaut', employed: true },
+    { name: 'Mark', age: 22, job: 'Basketball player', employed: true }
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'},
+    { key: 'employed', label: 'Employed'}
+  ];
 
   constructor() { }
 
