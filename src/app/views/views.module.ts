4128065ea44e1b2e1cac8e3da0559eb4596308b2
Leonardo Gonçalves <leomcg@gmail.com>
2020-06-15 14:12:03 -0300

    Create views module

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ViewsHomeComponent } from './views-home/views-home.component';
+import { SharedModule } from '../shared/shared.module';
+import { ItemlistComponent } from './itemlist/itemlist.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [StatisticsComponent, ViewsHomeComponent, ItemlistComponent],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
